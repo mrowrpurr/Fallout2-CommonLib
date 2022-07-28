02dc44ff1c13fed8e5a22bc754f5dd7b3fc7b811
Mrowr Purr <mrowr.purr@gmail.com>
2022-07-27 17:26:51 -0700

    #include "Common\FormattedDisplay.h"

---
+++ scripts/headers/Common/FormattedDisplay.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "sfall/lib.arrays.h"
+#include "sfall/lib.strings.h"
+
+#define __formatted_display_output(output) display_msg(output)
+
+#include "Common/Private/FormattedDisplayCore.h"
