71cfa9ccd89f5cdf85133e3a20ab98bc399da26e
Mrowr Purr <mrowr.purr@gmail.com>
2022-08-02 18:36:18 -0700

    Fix font_line_height bug in Common/UI/Fonts.h

--- scripts/headers/Common/UI/Fonts.h
+++ scripts/headers/Common/UI/Fonts.h
@@ -6,9 +6,10 @@
 #define all_valid_fonts ([100, 101, 102, 103, 104])
 
 #define font_line_height(font) \
-    10 if font == 101 else ( \
-    12 if font == 100 else ( \
-    28 if font == 102 else ( \
-    20 if font == 103 else ( \
-    26 if font == 104 else 0 \
-))))
+    ( \
+        10 if font == 101 else ( \
+        12 if font == 100 else ( \
+        28 if font == 102 else ( \
+        20 if font == 103 else ( \
+        26 if font == 104 else 0 \
+    )))))
