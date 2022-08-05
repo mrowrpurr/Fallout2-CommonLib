f1d3c3c0e8d0981f9f94874c38ec6c683fd25b97
Mrowr Purr <mrowr.purr@gmail.com>
2022-08-05 07:07:09 -0700

    max_lines and scroll_lines

--- scripts/headers/Common/UI/TextArea.h
+++ scripts/headers/Common/UI/TextArea.h
@@ -19,32 +19,134 @@
 #define __TEXT_AREA_DEFAULT_BACKGROUND_NORMALIZED_RGB_COLOR "000000000"
 #define __TEXT_AREA_DEFRAULT_WINDOW_NAME_PREFIX "CommonLibUiTextArea_"
 
-// @private
+// @private procedure declarations
 procedure __TextArea_AddVisibleLine(variable text_area, variable line_text, variable normalized_line_color);
+procedure __TextArea_Initialize(variable text_area);
 
-// Adds a line (using the default line color)
-procedure TextArea_AddLine(variable text_area, variable text) begin
-    call array_push(text_area._original_lines, text);
-    call array_push(text_area._original_line_colors, text_area.color);
-    call __TextArea_AddVisibleLine(text_area, text, text_area.color);
+#define TextArea_LineCount(text_area) (text_area._visible_line_count)
+
+procedure TextArea_Refresh(variable text_area) begin
+    if text_area and text_area.initialized then begin
+
+        if text_area.transparent then begin
+            DeleteWin(text_area.name);
+            call __TextArea_Initialize(text_area);
+            SelectWin(text_area.name);
+        end else begin
+            SelectWin(text_area.name);
+            variable background_color = text_area.background_color;
+            FillWin(
+                rgb_extract_red_float(background_color),
+                rgb_extract_green_float(background_color),
+                rgb_extract_blue_float(background_color)
+            );
+        end
+
+        variable line_count = text_area._visible_line_count;
+        if not line_count then begin
+            ShowWin;
+            return;
+        end
+        
+        SetFont(text_area.font);
+
+        variable width = text_area.width;
+        variable line_height = text_area.line_height;
+        variable last_color;
+        variable line_index = text_area._visible_lines_start;
+
+        variable line_number;
+        for line_number = 0; line_number < line_count; line_number++; begin
+            display_msg("RENDER LINE " + line_index);
+            variable line = text_area._visible_lines[line_index];
+            variable font_color = text_area._visible_line_colors[line_index];
+            if font_color != last_color then
+                SetTextColor(
+                    rgb_extract_red_float(font_color),
+                    rgb_extract_green_float(font_color),
+                    rgb_extract_blue_float(font_color)
+                );
+            last_color = font_color;
+            Format(
+                line, 
+                0,
+                (line_height * line_number),
+                width,
+                line_height,
+                justifyleft
+            );
+            line_index++;
+        end
+        ShowWin;
+    end
 end
 
-// Adds a line using provided color (formatted as HTML hex color string)
-procedure TextArea_AddColoredLine(variable text_area, variable text, variable hex_color) begin
-    variable normalized_rgb_color = rgb_normalize_hex(hex_color);
-    call array_push(text_area._original_lines, text);
-    call array_push(text_area._original_line_colors, normalized_rgb_color);
-    call __TextArea_AddVisibleLine(text_area, text, normalized_rgb_color);
+procedure TextArea_Show(variable text_area) begin
+    if text_area then begin
+        if text_area.visible then return;
+        text_area.visible = true;
+        if not text_area.initialized then
+            call __TextArea_Initialize(text_area);
+        show_window(text_area.name);
+        call TextArea_Refresh(text_area);
+    end
 end
 
-// Clears all lines (visible and non-visible)
-procedure TextArea_ClearLines(variable text_area) begin
-    resize_array(text_area._original_lines, 0);
-    resize_array(text_area._original_line_colors, 0);
-    resize_array(text_area._visible_lines, 0);
-    resize_array(text_area._visible_line_colors, 0);
-    text_area._visible_lines_start_index = 0;
-    text_area._visible_line_count = 0;
+procedure TextArea_Hide(variable text_area) begin
+    if text_area and text_area.visible then begin
+        text_area.visible = false;
+        hide_window(text_area.name);
+    end
+end
+
+procedure TextArea_ToggleVisibility(variable text_area) begin
+    if text_area then begin
+        if text_area.visible then
+            call TextArea_Hide(text_area);
+        else
+            call TextArea_Show(text_area);
+    end
+end
+
+// if line count is positive, the area is scrolled down that number of lines
+// if the line count is negative, the area is scrolled up the number of lines
+procedure TextArea_Scroll(variable text_area, variable lines_count) begin
+    if text_area then begin
+        variable scroll_lines = text_area.scroll_lines;
+        variable current_line_start_index = text_area._visible_lines_start;
+        variable all_visible_lines_count = len_array(text_area._visible_lines);
+
+        // Don't scroll until we have enough content to scroll
+        if all_visible_lines_count < scroll_lines then return;
+
+        if lines_count > scroll_lines then
+            lines_count = scroll_lines;
+        else if lines_count < 0 and lines_count < (scroll_lines * -1) then
+            lines_count = (scroll_lines * -1);
+
+        variable new_start = current_line_start_index + lines_count;
+        if new_start < 0 then
+            new_start = 0;
+        else if new_start > all_visible_lines_count - 1 then
+            new_start = all_visible_lines_count - 1;
+
+        text_area._visible_lines_start = new_start;
+    end
+end
+
+
+
+
+
+
+// scroll up one full visible page (or text_area.scroll_lines if defined)
+procedure TextArea_ScrollUp(variable text_area) begin
+
+end
+
+// scroll up one full visible page (or text_area.scroll_lines if defined)
+procedure TextArea_ScrollDown(variable text_area) begin
+
 end
 
 procedure TextArea_Create(variable defaults = 0) begin
@@ -77,8 +179,13 @@ procedure TextArea_Create(variable defaults = 0) begin
     if not text_area.line_height or text_area.line_height < height_of_font then
         text_area.line_height = height_of_font;
 
-    // TODO: make the fields which store lines and colors "private" by staring with .__
-    // TODO: rename all_lines/line_colors to something like all_line_colors for consistency with visible* fields
+    variable max_visible_lines = floor(text_area.height / text_area.line_height);
+    if not text_area.max_lines or text_area.max_lines > max_visible_lines then
+        text_area.max_lines = max_visible_lines;
+
+    variable max_lines = text_area.max_lines;
+    if not text_area.scroll_lines or text_area.scroll_lines > max_lines then
+        text_area.scroll_lines = max_lines;
 
     // Stores all lines 
     text_area._original_lines = [];
@@ -98,54 +205,71 @@ procedure TextArea_Create(variable defaults = 0) begin
     text_area._visible_line_colors = [];
     fix_array(text_area._visible_line_colors);
 
-    text_area._visible_lines_start_index = 0;
-    text_area._visible_line_count   = 0;
+    text_area._visible_lines_start = 0;
+    text_area._visible_line_count = 0;
 
     return text_area;
 end
 
-procedure TextArea_DestroyUI(variable text_area) begin
-    
-end
-
-procedure TextArea_Show(variable text_area) begin
-    // text_area.visible = true;
-    // log_info("SHOW WINDOW '" + WINDOW_NAME + "'");
-    // create_win_flag(
-    //         WINDOW_NAME,
-    //         10,
-    //         100,
-    //         500,
-    //         400,
-    //         WIN_FLAG_HIDDEN + WIN_FLAG_MOVEONTOP
-    //     );
-    // SelectWin(WINDOW_NAME);
-    // FillWin(0.0, 0.0, 0.0);
-    // SetFont(101);
-    // SetTextColor(0.988, 0, 0.957);
-    // Format("Hey wassup? a bunch of text yo!", 5, 10, 240, 12, justifyleft);
-    // show_window(WINDOW_NAME);
-end
-
-procedure TextArea_Hide(variable text_area) begin
-    // DeleteWin(WINDOW_NAME); // check if it exists first
-    text_area.visible = false;
-end
-
-procedure TextArea_ToggleVisibility(variable text_area) begin
+procedure TextArea_Destroy(variable text_area) begin
     if text_area then begin
-        if text_area.visible then
-            call TextArea_Hide(text_area);
-        else
-            call TextArea_Show(text_area);
+        if text_area.initialized then DeleteWin(text_area.name); // check if it exists first
+        free_array(text_area._original_lines);
+        free_array(text_area._original_line_colors);
+        free_array(text_area._visible_lines);
+        free_array(text_area._visible_line_colors);
     end
 end
 
-procedure TextArea_Destroy(variable text_area) begin
-
+// Adds a line (using the default line color)
+procedure TextArea_AddLine(variable text_area, variable text) begin
+    call array_push(text_area._original_lines, text);
+    call array_push(text_area._original_line_colors, text_area.color);
+    call __TextArea_AddVisibleLine(text_area, text, text_area.color);
+    if text_area.visible then call TextArea_Refresh(text_area);
 end
 
-// @private
+// Adds a line using provided color (formatted as HTML hex color string)
+procedure TextArea_AddColoredLine(variable text_area, variable text, variable hex_color) begin
+    variable normalized_rgb_color = rgb_normalize_hex(webcolor_hex(hex_color));
+    call array_push(text_area._original_lines, text);
+    call array_push(text_area._original_line_colors, normalized_rgb_color);
+    call __TextArea_AddVisibleLine(text_area, text, normalized_rgb_color);
+    if text_area.visible then call TextArea_Refresh(text_area);
+end
+
+// Clears all lines (visible and non-visible)
+procedure TextArea_Clear(variable text_area) begin
+    resize_array(text_area._original_lines, 0);
+    resize_array(text_area._original_line_colors, 0);
+    resize_array(text_area._visible_lines, 0);
+    resize_array(text_area._visible_line_colors, 0);
+    text_area._visible_lines_start = 0;
+    text_area._visible_line_count = 0;
+    if text_area.visible then call TextArea_Refresh(text_area);
+end
+
+/*
+    @private procedures
+*/
+
+inline procedure __TextArea_Initialize(variable text_area) begin
+    variable flags;
+    if text_area.transparent then
+        flags = WIN_FLAG_HIDDEN + WIN_FLAG_MOVEONTOP + WIN_FLAG_TRANSPARENT;
+    else
+        flags = WIN_FLAG_HIDDEN + WIN_FLAG_MOVEONTOP;
+    create_win_flag(
+        text_area.name,
+        text_area.x,
+        text_area.y,
+        text_area.width,
+        text_area.height,
+        flags
+    );
+    text_area.initialized = true;
+end
+
 // Called when adding lines via TextArea_AddLine or TextArea_AddColoredLine
 // Inline because it's only used from 2 places (adding color and non-color lines)
 // and would prefer not to add an additional procedure invocation on line addition.
@@ -177,10 +301,10 @@ inline procedure __TextArea_AddVisibleLine(variable text_area, variable line_tex
         current_total_line_height = len_array(text_area._visible_lines) * line_height;
         available_vertical_space  = text_area.height - current_total_line_height;
 
-        if available_vertical_space - line_height >= 0 then
+        if text_area._visible_line_count < text_area.max_lines and available_vertical_space - line_height >= 0 then
             text_area._visible_line_count++; // There is enough visible space to display this line!
         else if autoscroll then
-            text_area._visible_lines_start_index++; // Move the index of visible lines down
+            text_area._visible_lines_start++; // Move the index of visible lines down
 
         call array_push(text_area._visible_lines, text_to_add);
         call array_push(text_area._visible_line_colors, normalized_line_color);
