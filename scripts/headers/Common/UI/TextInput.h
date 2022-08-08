f418121a16203798db89fe4768e01140ad21c409
Mrowr Purr <mrowr.purr@gmail.com>
2022-08-07 17:10:57 -0700

    Get basics of TextInput entry/visibility working

--- scripts/headers/Common/UI/TextInput.h
+++ scripts/headers/Common/UI/TextInput.h
@@ -7,28 +7,39 @@
 #include "Common/UI/TextArea.h"
 #include "Common/Keyboard/ScanCodeToText.h"
 
-procedure TextInput_Create begin
-    variable input = {};
-    fix_array(input);
+#define KEYCODE_WHICH_DOES_NOTHING (255)
 
-    input.textarea = TextArea_Create({
-        "font": 101,
+procedure TextInput_Create begin
+    variable text_input = {};
+    fix_array(text_input);
+
+    text_input.textarea = TextArea_Create({
+        "font": 103,
         "max_lines": 1,
-        "height": 15,
-        "color": "green"
+        "height": 25,
+        "color": "hot pink"
     });
 
-    input.text = "";
-
-    call TextArea_Show(input.textarea);
-
-    return input;
+    text_input.text = "$ ";
+    return text_input;
 end
 
-procedure TextInput_InputKeyCode(variable input, variable keycode) begin
-    display_msg("KEYCODE: " + keycode);
-    variable key_text = scan_code_to_text(keycode);
-    input.text += key_text;
-    call TextArea_Clear(input.textarea);
-    call TextArea_AddLine(input.textarea, input.text);
+procedure TextInput_InputKeyCode(variable text_input, variable keycode) begin
+    variable scan_code = scan_code_to_text(keycode);
+    if not scan_code.visible then return;
+    text_input.text += scan_code.text;
+    call TextArea_Clear(text_input.textarea);
+    call TextArea_AddLine(text_input.textarea, text_input.text);
+end
+
+procedure TextInput_OnKeypress(variable text_input, variable pressed, variable dx_scan_code) begin
+    if text_input.textarea.visible then begin
+        if pressed then call TextInput_InputKeyCode(text_input, dx_scan_code);
+        return KEYCODE_WHICH_DOES_NOTHING;
+    end else
+        return dx_scan_code;
+end
+
+procedure TextInput_ToggleVisibility(variable text_input) begin
+    call TextArea_ToggleVisibility(text_input.textarea);
 end
