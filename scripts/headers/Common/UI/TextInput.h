f32499f600a934bfce4664dcffce655632eedd4f
Mrowr Purr <mrowr.purr@gmail.com>
2022-08-08 18:58:26 -0700

    Support shift key

--- scripts/headers/Common/UI/TextInput.h
+++ scripts/headers/Common/UI/TextInput.h
@@ -9,23 +9,25 @@
 
 #define KEYCODE_WHICH_DOES_NOTHING (255)
 
-procedure TextInput_Create begin
-    variable text_input = {};
+procedure TextInput_Create(variable parameters = 0) begin
+    variable text_input = {
+        "text": ""
+    };
     fix_array(text_input);
 
-    text_input.textarea = TextArea_Create({
-        "font": 103,
-        "max_lines": 1,
-        "height": 25,
-        "color": "hot pink"
-    });
+    if not parameters then parameters = {};
+    parameters.max_lines = 1;
 
-    text_input.text = "$ ";
+    // Hacky, for now just pass parameters straight to the TextArea
+    text_input.textarea = TextArea_Create(parameters);
+
+    text_input.textarea.height = text_input.textarea.line_height;
+
     return text_input;
 end
 
 procedure TextInput_InputKeyCode(variable text_input, variable keycode) begin
-    variable scan_code = scan_code_to_text(keycode);
+    variable scan_code = scan_code_to_text(keycode, text_input.is_shift_pressed);
     if not scan_code.visible then return;
     text_input.text += scan_code.text;
     call TextArea_Clear(text_input.textarea);
@@ -33,6 +35,10 @@ procedure TextInput_InputKeyCode(variable text_input, variable keycode) begin
 end
 
 procedure TextInput_OnKeypress(variable text_input, variable pressed, variable dx_scan_code) begin
+    // if is_shift_key_code(dx_scan_code) <-- TODO
+    if dx_scan_code == 42 then
+        text_input.is_shift_pressed = pressed;
+
     if text_input.textarea.visible then begin
         if pressed then call TextInput_InputKeyCode(text_input, dx_scan_code);
         return KEYCODE_WHICH_DOES_NOTHING;
